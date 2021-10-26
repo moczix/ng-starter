cafe8c0e02ec570274362baea4f6fc995a112d1a
Krol Frontu <michal.jamrozy@beefee.co.uk>
2021-10-26 18:02:58 +0200

    fix linters

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ require('jest-preset-angular/ngcc-jest-processor');
 const { pathsToModuleNameMapper } = require('ts-jest/utils')
 const { compilerOptions } = require('./tsconfig')
 
-
 module.exports = {
   preset: 'jest-preset-angular',
   transform: {
